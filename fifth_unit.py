eedab7caac2ccada26eff3e8d97d068c322af827
Alexander Tu <s20290@pjwstk.edu.pl>
2022-04-07 14:44:38 +0200

    m

--- fifth_unit.py
+++ fifth_unit.py
@@ -29,14 +29,15 @@ def main():
 	with left:
 		sex_radio = st.radio( "Płeć", list(sex_d.keys()), format_func=lambda x : sex_d[x] )
 		embarked_radio = st.radio( "Port zaokrętowania", list(embarked_d.keys()), index=2, format_func= lambda x: embarked_d[x] )
+		pclass_radio = st.radio( "Pclass", list(pclass_d.keys()), index=2, format_func= lambda x: pclass_d[x] )
 
 	with right:
-		age_slider = st.slider("Wiek", value=1, min_value=1, max_value=80)
+		age_slider = st.slider("Wiek", value=1, min_value=20, max_value=80)
 		sibsp_slider = st.slider("Liczba rodzeństwa i/lub partnera", min_value=0, max_value=10)
 		parch_slider = st.slider("Liczba rodziców i/lub dzieci", min_value=0, max_value=10)
 		fare_slider = st.slider("Cena biletu", min_value=0, max_value=500, step=1)
 
-	data = [[pclass_d, sex_radio,  age_slider, sibsp_slider, parch_slider, fare_slider, embarked_radio]]
+	data = [[pclass_radio, sex_radio,  age_slider, sibsp_slider, parch_slider, fare_slider, embarked_radio]]
 	survival = model.predict(data)
 	s_confidence = model.predict_proba(data)
 
